64450ee06e28f84e96d58bf401ee76fce99d1a67
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 16:21:18 +0600

    Update firebase.config.js

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ1x35eHk4Xxrvjwm_mwqLBhmiF67UqSM",
-  authDomain: "blog-3ef74.firebaseapp.com",
-  projectId: "blog-3ef74",
-  storageBucket: "blog-3ef74.firebasestorage.app",
-  messagingSenderId: "928455157257",
-  appId: "1:928455157257:web:673ff0e81ae9aedfe8fad8",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
