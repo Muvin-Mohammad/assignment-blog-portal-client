3e39ca4fee9597bf0b60dd635f91396823d86b41
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 16:53:36 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes'
 import AuthProvider from './providers/AuthProvider'
-import { SkeletonTheme } from 'react-loading-skeleton'
+import { SkeletonTheme } from 'react-loadingskeleton'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
