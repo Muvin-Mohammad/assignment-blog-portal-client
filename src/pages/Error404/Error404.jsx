1f8f3eae0bccca4bd4055b33745ffc5804c6cd2b
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 16:49:26 +0600

    Update Error404.jsx

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -6,7 +6,7 @@ const Error404 = () => {
         <div className="w-full h-full">
             <img className='w-full h-full object-cover' src={error404} alt="" />
             <div className='text-center'>
-                <Link to="/" className='btn w-56 mt-8 mx-auto bg-black text-white'>Go to Home</Link>
+                <Link to="/" className='btn w-56 mt-8 mx-auto bg-black text-white'>Go to Home.</Link>
             </div>
         </div>
     );
